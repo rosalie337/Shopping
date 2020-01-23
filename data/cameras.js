c47c2b8b63743d55189cfd794c8781acf16d6f6e
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-01-22 16:43:27 -0800

    home and products page

---
+++ data/cameras.js
@@ -0,0 +1,85 @@
+const nikonZ6 = {
+    id: 'Camera 1',
+    name: 'Nikon Z6',
+    image: 'Nikon Z6.jpg',
+    description:'FX-Format Mirrorless Camera Body',
+    category: 'Nikon',
+    price: 1689.00,
+};
+
+const fujiFilmX = {
+    id: 'Camera 2',
+    name: 'Fujifilm X-T30',
+    image: 'Fujifilm - X Series X-T3.jpg',
+    description:'Mirrorless Camera with XF18-55mm F2.8-4 R LM OIS Lens - Silver',
+    category: 'Fujifilm',
+    price: 749.00,
+
+};
+
+const sonyA = {
+    id:'Camera 3',
+    name:'Sony A7 III',
+    image:'Sony A7 III.png',
+    description:'Full frame sensors, 3.0-inch tilt-angle touchscreen',
+    category:'Sony',
+    price: 1589.00,
+}; 
+
+const nikonD850 = {
+    id:'Camera 4',
+    name:'Nikon D850',
+    image:'Nikon D850 DSLR.img',
+    description:'Nikon D850 DSLR Camera Body',
+    category:'Nikon',
+    price: 2295.00,
+};
+
+const nikonZ50 = {
+    id:'Camera 5'
+    name:'Nikon Z50',
+    image:'Nikon Z50.png',
+    description:'Nikon Z50 mid-range mirrorless upgrade for DSLR owners',
+    category:'Nikon',
+    price: 856.95,
+};
+
+const olympusOm = {
+    id:'Camera 6',
+    name:'Olympus OM-D E-M10 Mark III',
+    image:'Olympus OM.jpg',
+    description:'Olympus OM-D E-M10 Mark III',
+    category:'Olympus',
+    price: 479.99,
+};
+
+const canonEosRebel = {
+    id:'Camera 7',
+    name:'Canon EOS Rebel SL3 / EOS 250D',
+    image:' Canon EOS Rebel.jpg',
+    description:'Looking for a first DSLR? The Rebel SL2 ticks plenty of boxes',
+    category:'Canon'
+    price: 539.00,
+};
+
+const panasonicLumix = {
+    id:'Camera 8',
+    name:'Panasonic Lumix ZS200 / TZ200',
+    image:'Panasonic Lumix.jpg',
+    description:'The perfect travel camera - small, versatile and with a decent zoom',
+    category:'Panasonic',
+    price: 697.99,
+};
+
+const cameras = {
+    nikonZ6,
+    fujiFilmX,
+    sonyA,
+    nikonD850
+    nikonZ50,
+    olympusOm,
+    canonEosRebel,
+    panasonicLumix,
+};
+
+export default cameras;
---
+++ product/create-products.js
@@ -0,0 +1,35 @@
+function createdProduct(camera) {
+    const li = document.createElement('li');
+    li.className = camera.category;
+    li.title = camera.description;
+
+    const h2 = document.createElement('h2');
+    h3.textContent = camera.name;
+    li.appendChild(h2);
+
+    const img = document.createElement('img');
+    img.src = '../assets/' + camera.image;
+    img.alt = camera.name + 'image';
+    li.appendChild(img);
+
+    const p = document.createElement('p');
+    p.className = 'price';
+
+    const usd = '$' + camera.price.toFixed(2);
+    // const usd = camera.price.toLocaleString('en-US', { 
+    //     style: 'currency', 
+    //     currency: 'USD' 
+    // });
+    p.textContent = usd;
+    
+    const button = document.createElement('button');
+    button.textContent = 'Add';
+    button.value = camera.code;
+    p.appendChild(button);
+
+    li.appendChild(p);
+
+    return li;
+}
+
+export default createdProduct;
