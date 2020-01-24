b4e2b84ca09eed96ea6debad9c1fa93dea4d9fd7
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-01-23 22:23:21 -0800

    Lab 07 add cart in process

---
+++ data/cart.js
@@ -0,0 +1,27 @@
+const cart = [{
+    id:'Camera 1',
+    quantity: 2
+}, {
+    id:'Camera 2',
+    quantity: 4
+}, {
+    id:'Camera 3',
+    quantity: 6
+}, {
+    id:'Camera 4',
+    quantity: 8
+}, {
+    id:'Camera 5',
+    quantity: 10
+}, {
+    id:'Camera 6',
+    quantity: 12
+}, {
+    id:'Camera 7',
+    quantity: 14
+}, {
+    id:'Camera 8',
+    quantity: 9
+}];
+
+export default cart;
